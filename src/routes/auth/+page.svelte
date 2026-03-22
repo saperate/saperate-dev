ca43f28acbd7fdb380c17075192c971cdc595395
saperate <61527642+saperate@users.noreply.github.com>
2026-03-22 07:21:55 -0400

    oopsie daisy

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,41 +1,6 @@
-<script lang="ts">
-	import HankoAuth from "../components/HankoAuth.svelte";
-	import LogoutButton from "../components/LogoutButton.svelte";
+<script>
+	import HankoAuth from "../../components/HankoAuth.svelte";
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
 THIS IS AUTH
 <HankoAuth/>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
